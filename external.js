efb8fe098ba965af38053ac88389937c4eb544eb
uday <udaygitlog@gmail.com>
2022-09-02 22:48:55 +0530

    Completed playRound() function

--- external.js
+++ external.js
@@ -1,4 +1,6 @@
 gameArray = ['Rock','Paper','Scissors']
+const playerSelection = prompt("Please Enter your choice!","rock");
+const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     const randomElement = gameArray[Math.floor(Math.random()*gameArray.length)];
@@ -8,23 +10,21 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let lowerPlayer = playerSelection.toLowerCase()
     
-    if (lowerPlayer == computerSelection) {
-        console.log("It's a tie!")
+    if (lowerPlayer == computerSelection.toLowerCase()) {
+        return "It's a tie!"
     }
 
-    else if (
-        (lowerPlayer=="rock" && computerSelection=="Scissors")
-        || (lowerPlayer=="scissors" && computerSelection=="Paper")
-        || (lowerPlayer=="Paper" && computerSelection=="Rock")
-        ) {
-        console.log("You Win!")
+    else if ((lowerPlayer=="rock" && computerSelection=="Scissors") || (lowerPlayer=="scissors" && computerSelection=="Paper") || (lowerPlayer=="paper" && computerSelection=="Rock")) {
+        return "You Win!"
+    }
+
+    else if (lowerPlayer!=gameArray[0,1,2]) {
+        return "Try again"
     }
 
     else {
-        console.log("You Lose! lol noob.")
+        return "You Lose! lol noob."
     }
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
