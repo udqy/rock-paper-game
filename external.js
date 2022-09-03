09c614c49bf5aaefcce79fd516cd025e5298d570
uday <udaygitlog@gmail.com>
2022-09-03 11:18:32 +0530

    Redo playRound function

--- external.js
+++ external.js
@@ -1,29 +1,48 @@
 gameArray = ['Rock','Paper','Scissors']
-const playerSelection = prompt("Please Enter your choice!","rock");
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice()
+let playerSelection = prompt("Enter","Rock")
 
 function getComputerChoice() {
     const randomElement = gameArray[Math.floor(Math.random()*gameArray.length)];
     return randomElement
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection = prompt("Enter","rock"), computerSelection) {
     let lowerPlayer = playerSelection.toLowerCase()
     
     if (lowerPlayer == computerSelection.toLowerCase()) {
-        return "It's a tie!"
+        console.log("It's a tie!")
     }
 
-    else if ((lowerPlayer=="rock" && computerSelection=="Scissors") || (lowerPlayer=="scissors" && computerSelection=="Paper") || (lowerPlayer=="paper" && computerSelection=="Rock")) {
-        return "You Win!"
+    else if (lowerPlayer=="rock") {
+        if (computerSelection == "Paper") {
+            return "You lose! Paper beats Rock."
+        }
+        else if (computerSelection == "Scissors") {
+            return "You Win! Rock beats Scissors."
+        }
+    }
+
+    else if (lowerPlayer=="paper") {
+        if (computerSelection == "Rock") {
+            return "You Win! Paper beats Rock."
+        }
+        else if (computerSelection == "Scissors") {
+            return "You lose! Scissor beats Paper."
+        }
+    }
+
+    else if (lowerPlayer=="scissors") {
+        if (computerSelection == "Paper") {
+            return "You Win! Scissor beats Paper"
+        }
+        else if (computerSelection == "Rock") {
+            return "You lose! Rock beats Scissor."
+        }
     }
 
     else if (lowerPlayer!=gameArray[0,1,2]) {
-        return "Try again"
-    }
-
-    else {
-        return "You Lose! lol noob."
+        console.log("Try again!")
     }
 }
 
